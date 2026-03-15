2da0c64ea7d6b7f10f8192a81cf4e1f703db4351
wheelzack <50170853+wheelzack@users.noreply.github.com>
2026-03-15 12:06:38 +0530

    Implement Tensor class with basic operations

---
+++ engine.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class Tensor:
+    """ A mini-tensor with autograd, optimized for Sensora Labs GPT """
+    def __init__(self, data, _children=(), _op=''):
+        self.data = np.array(data)
+        self.grad = np.zeros_like(self.data)
+        self._backward = lambda: None
+        self._prev = set(_children)
+        self._op = _op
+
+    def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
+
+    def __matmul__(self, other):
+        out = Tensor(self.data @ other.data, (self, other), '@')
+        def _backward():
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
+        out._backward = _backward
+        return out
+
+    def relu(self):
+        out = Tensor(np.maximum(0, self.data), (self,), 'ReLU')
+        def _backward():
+            self.grad += (self.data > 0) * out.grad
+        out._backward = _backward
+        return out
+
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = np.ones_like(self.data)
+        for v in reversed(topo):
+            v._backward()
